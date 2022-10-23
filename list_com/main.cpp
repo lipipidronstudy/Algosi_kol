bfe14ebb86e63361dfbfe3a73588e9153d75ea3f
name <naaasmesurrmmane@gmail.com>
2022-10-23 13:34:52 +0300

    added add

--- list_com/main.cpp
+++ list_com/main.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 
+using namespace std;
+
 struct Node {
     int data;
     Node *next;
     Node *prev;
 };
 
+
 struct myList {
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
 
     myList() {
         head = tail = nullptr;
     }
-};
 
-using namespace std;
+    void addToHead(int x) {
+        Node* tmp = new Node;
+        tmp -> data = x;
+
+        tmp -> next = head;
+        tmp -> prev = nullptr;
+        head -> prev = tmp;
+        head = tmp;
+    }
+
+    void addToTail(int x) {
+        Node* tmp = new Node;
+        tmp -> data = x;
+
+        tmp -> prev = tail;
+        tmp -> next = nullptr;
+        tail -> next = tmp;
+        tail = tmp;
+    }
+};
 
 int main() {
 
